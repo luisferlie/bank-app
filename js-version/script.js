f8c70f526f259ff5585ee99a233b387cc3436220
luisferlie <luisferlie@gmail.com>
2024-02-13 12:33:42 +0100

    primer commit

--- js-version/script.js
+++ js-version/script.js
@@ -60,3 +60,5 @@ const inputTransferAmount = document.querySelector('.form__input--amount');
 const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
+
+test; 
